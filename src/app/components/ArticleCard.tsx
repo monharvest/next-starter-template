d879b44c5c54fcc17b5bedbb1a25ccf751155da3
monharvest <112895060+monharvest@users.noreply.github.com>
2025-08-30 13:48:41 +0200

    Update ArticleCard.tsx

--- src/app/components/ArticleCard.tsx
+++ src/app/components/ArticleCard.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 
-export default function ArticleCard({article}:{article:any}){
+type Article = {
+  title: string;
+  excerpt: string;
+  image: string;
+  date: string;
+  author: string;
+  category?: string;
+};
+
+export default function ArticleCard({ article }: { article: Article }) {
   return (
     <article className="article-card">
-      <img src={article.image} alt={article.title}/>
+      <img src={article.image} alt={article.title} />
       <div>
         <div className="meta">{article.date} • {article.author}</div>
         <h4>{article.title}</h4>
         <p>{article.excerpt}</p>
       </div>
     </article>
-  )
+  );
 }
